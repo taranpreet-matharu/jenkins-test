1c7d8bf4638f8d6b093a85d17eb8c185dd36a4d4
Taranpreet Kaur <taranpreet.matharu@gmail.com>
2020-04-26 23:36:00 -0700

    Third change push

--- index.js
+++ index.js
@@ -5,13 +5,13 @@ var path = require('path');
 
 app.get('/hello', (req, res) => {
     console.log('Hello..!!');
-    res.send('Hello..2!!');
+    res.send('Hello..3!!');
 });
 
 
 app.listen(2000, (err) => {
     if (err) {
-        console.log(`Error in startig server: ${err}`);
+        console.log(`Error in starting server: ${err}`);
     }
     console.log('Server listening on port 2000...');
 });
