43563b2cf66cebd8ae2faaaee610822ed893bace
Taranpreet Kaur <taranpreet.matharu@gmail.com>
2020-04-26 23:03:08 -0700

    First Commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+var path = require('path');
+
+
+app.get('/hello', (req, res) => {
+    console.log('Hello..!!');
+    res.send('Hello..!!');
+});
+
+
+app.listen(2000, (err) => {
+    if (err) {
+        console.log(`Error in startig server: ${err}`);
+    }
+    console.log('Server listening on port 2000...');
+});
